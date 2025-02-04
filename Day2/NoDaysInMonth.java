36804b668b699028b7970b6fdaef878ed3815f99
muhmdpp <connect.mhdpp@gmail.com>
2025-02-04 15:14:30 +0530

    Evening Session

--- Day2/NoDaysInMonth.java
+++ Day2/NoDaysInMonth.java
@@ -2,10 +2,11 @@ import java.util.Scanner;
 
 public class NoDaysInMonth {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int month=2;
-        int days=0;
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Month");
+        int month =sc.nextInt();
+        int days = 0;
+
         switch (month) {
             case 1:
             case 3:
@@ -13,26 +14,32 @@ public class NoDaysInMonth {
             case 7:
             case 8:
             case 10:
-            case 12:days=31;
-            break;
+            case 12:
+                days = 31;
+                break;
             case 4:
             case 6:
             case 9:
-            case 11:days=30;
-            break;
+            case 11:
+                days = 30;
+                break;
             case 2:
-            System.out.println("Enter Year:");
-            int year =sc.nextInt();
-            if(year%4==0 && year%100!=0 || year%400==0){
-                days=29;
-            }else{
-                days=28;
-            }
-            sc.close();
+                System.out.println("Enter Year:");
+                int year = sc.nextInt();
+                // using ternary Operator
+                days = (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) ? (days = 29) : (days = 28);
+
+                // days=29;
+                // }else{
+                // days=28;
+                // }
+                sc.close();
+                break;
             default:
+                System.out.println("Invalid Month");
                 break;
         }
 
-        System.out.println("days are:"+days);
+        System.out.println("days are:" + days);
     }
 }
---
+++ Day2/UserInput.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class UserInput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // integer
+        System.out.println("Enter the Integer");
+        int a = sc.nextInt();
+        
+
+        // float
+        System.out.println("Enter the Float Value");
+        float f = sc.nextFloat();
+        System.out.println(f);
+
+        // String
+        System.out.println("Enter String");
+        // sc.nextLine();
+        String s= sc.nextLine();
+        System.out.println(a);
+        System.out.println(s);
+
+
+    }
+}
