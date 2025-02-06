888644903536d66ec181cd19f465ef3f15453b5d
muhmdpp <connect.mhdpp@gmail.com>
2025-02-06 11:49:17 +0530

    DAY4-FN

---
+++ Day4/DoWhile.java
@@ -0,0 +1,10 @@
+package Day4;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        int n = 1;
+        do {
+            System.out.println("hello World");
+        } while (n >= 5);
+    }
+}
