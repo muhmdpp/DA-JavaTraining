9cab4003cbcaa4bd435fbabb9ac6b61278adde52
muhmdpp <connect.mhdpp@gmail.com>
2025-02-07 15:37:50 +0530

    DAY5-EVENING

---
+++ Assignments/NeonNumber.java
@@ -0,0 +1,18 @@
+package Assignments;
+
+public class NeonNumber {
+    public static void main(String[] args) {
+        int n=9;
+        int sqr=n*n;
+        int temp=sqr;
+        int rem=0,sum=0;
+        
+        while (temp!=0) {
+            rem=temp%10;
+            sum+=rem;
+            temp/=10;
+
+        }
+        System.out.println(sum);
+    }
+}
