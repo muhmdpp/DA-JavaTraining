6ca8b2642309db3d426c253753b59e51b4d6bdc2
muhmdpp <connect.mhdpp@gmail.com>
2025-02-08 09:50:36 +0530

    DAY6-starting

---
+++ Day6/ThirdMin.java
@@ -0,0 +1,25 @@
+package Day6;
+
+public class ThirdMin {
+    public static void main(String[] args) {
+        int first = Integer.MAX_VALUE;
+        int second = Integer.MAX_VALUE;
+        int third = Integer.MAX_VALUE;
+        int[] a = { 4, 8, 3, 6, 1 };
+        for (int i = 0; i < a.length; i++) {
+
+            if(first>a[i]){
+                third=second;
+                second=first;
+                first=a[i];
+            }else if(second>a[i]){
+                third=second;
+                second=a[i];
+            }else if(third>a[i] && a[i]!=first && a[i]!=second){
+                third=a[i];
+            }
+        }
+
+        System.out.println(third);
+    }
+}
